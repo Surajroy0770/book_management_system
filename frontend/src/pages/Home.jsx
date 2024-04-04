62745f3753f8abbede3900ba619e896ff0e357bd
suraj roy <surajroy0770@gmail.com>
2024-04-04 16:26:18 +0530

    fourth update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,8 +5,8 @@ export const Home = () => {
   return (
     <div className="Home_container">
       <div className="lift_side">
-        <h1>BOOK STORE</h1>
-         <h1> FOR YOU</h1>
+        <h1 className='titles'>BOOK STORE</h1>
+         <h1 className='titles'> FOR YOU</h1>
          <Link  to="/books" >
         <button className='btn'>View Books</button>
         </Link>
