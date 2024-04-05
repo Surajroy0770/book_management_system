d0c274b2461a2a1d9d521906c7f523be5ada969d
suraj roy <surajroy0770@gmail.com>
2024-04-05 12:08:51 +0530

    update

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const ConnectMongo = () => {
-    mongoose.connect(process.env.MONGOOSE_URL)
+    mongoose.connect(process.env.MONGOOSE_URL,{dbname:"Book_appointment_system"})
         .then(() => {
             console.log("connected successfully")
         })
